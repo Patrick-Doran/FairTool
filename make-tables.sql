1cd3b637db89fc9f9e1b0f22850faa88e4e426fd
marco <mharo-crespo@ucmerced.edu>
2020-10-14 17:15:49 -0700

    make db

---
+++ make-tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE database(
+    user varchar(25) not null,
+    var1 decimal(1,0) not null,
+    var2 decimal(1,0) not null,
+    var3 decimal(1,0) not null,
+    var4 decimal(1,0) not null,
+    var5 decimal(1,0) not null,
+    var6 decimal(1,0) not null,
+    var7 decimal(1,0) not null,
+    var8 decimal(1,0) not null,
+    var9 decimal(1,0) not null,
+    var10 decimal(1,0) not null,
+    var11 decimal(1,0) not null,
+    var12 decimal(1,0) not null,
+    input_date date not null,
+    comments varchar(144)
+)
